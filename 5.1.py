e7ab2dbe3255009d7f5ba7459b0e318fcc7912f1
genn125 <genn125@gmail.com>
2024-12-29 13:15:48 +0300

    Начало

--- 5.1.py
+++ 5.1.py
@@ -1,4 +1,28 @@
 
-
+# 5.1
 
 # Атрибуты и методы объекта
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor +1):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
+# print(h1.name, ',', h1.number_of_floors)
+# print(h2.name, ',', h2.number_of_floors)
