00bc8a66088b07405d857215ee6379db60d8db89
genn125 <genn125@gmail.com>
2025-01-02 15:00:16 +0300

    Нужно больше этажей

--- 5.3.py
+++ 5.3.py
@@ -5,57 +5,60 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __str__(self):
+    def __str__(self): # 1 ,2
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
-    def __eq__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
+    def __eq__(self, other): # 3
+        return self.number_of_floors == other.number_of_floors
 
-    def __lt__(self, other):
+    def __lt__(self, other): # 10
             return self.number_of_floors < other.number_of_floors
 
-    def __le__(self, other):
+    def __le__(self, other): # 11
                 return self.number_of_floors <= other.number_of_floors
 
-    def __gt__(self, other):
+    def __gt__(self, other): # 8
         return self.number_of_floors > other.number_of_floors
 
-    def __ge__(self, other):
+    def __ge__(self, other): # 9
         return self.number_of_floors >= other.number_of_floors
 
-    def __ne__(self, other):
+    def __ne__(self, other): # 12
         return self.number_of_floors != other.number_of_floors
 
-    def __add__(self, value):
+    def __add__(self, value): # 4, 5
         if isinstance(value, int):
             self.number_of_floors += value
             return self
 
-    def __radd__(self, value):
+    def __iadd__(self, value): # 6
         return self.__add__(value)
 
-    def __iadd__(self, value):
+    def __radd__(self, value): # 7
         return self.__add__(value)
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-print(h1)
-print(h2)
+print('1 __str__', h1)
+print('2 __str__', h2)
 
-print(h1 == h2) # __eq__
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
+print('3 (__eq__)', h1 == h2)
+
+h1 += 10 # __add__
+print('4 (__add__)', h1)
+print('5 (__add__)', h1 == h2)
+
 h1 += 10 # __iadd__
-print(h1)
+print('6 (__iadd__)', h1)
+
 h2 = 10 + h2 # __radd__
-print(h2)
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print('7 (__radd__)', h2)
+
+print('8 __gt__', h1 > h2)
+print('9 __ge__', h1 >= h2)
+print('10 __lt__', h1 < h2)
+print('11 __le__', h1 <= h2)
+print('12 __ne__', h1 != h2)
 
 
